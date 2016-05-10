e3b010bf3ee7282cb15b3b4bcba21cc78a76325b
Baptiste Courtois <b.courtois@criteo.com>
2016-05-10 17:01:48 +0200

    Chef_supermarket provider needs mime-type ~> 2.0

--- lib/dpl/provider/chef_supermarket.rb
+++ lib/dpl/provider/chef_supermarket.rb
@@ -6,6 +6,7 @@ module DPL
       # https://github.com/opscode/chef/blob/11.16.4/lib/chef/knife/cookbook_site_share.rb
 
       # Compatibility with ruby 1.9
+      requires 'mime-types', version: '~> 1.16'
       requires 'chef', version: '< 12.0'
       requires 'chef', load: 'chef/config'
       requires 'chef', load: 'chef/cookbook_loader'
